1a21bc88ba13e577b9d94f3ab1b50fa5bfeda60c
Tushar975868 <tusharsharma975@yahoo.com>
2024-06-26 00:28:43 +0530

    Shifted app.js file data to HomePage.js to maintain routing in app.js

---
+++ src/compound/HomePage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Navbar from './Navbar';
+import MobileNavTabs from '../atom/MobileNavTabs';
+import { useSelector } from 'react-redux';
+import './css/HomePage.style.css';
+import homeBackground from '../atom/images/video/homeBackground.mp4';  //'./atom/images/video/homeBackground.mp4';
+import HomePageText from '../atom/HomePageText';
+import BusinessStrength from '../molecule/BusinessStrength';
+import CombineRing from '../molecule/CombineRing';
+import CoreCompitancy from '../molecule/CoreCompitancy';
+import Footer from './Footer';
+
+
+const HomePage = () => {
+
+  //selectors
+  const showMobileTab = useSelector((state) =>  state.showHideMobileNavBar )
+  console.log(showMobileTab, "Tushar")
+  return (
+    <div className='default-class'>
+
+      <Navbar/>
+
+      {showMobileTab && <MobileNavTabs />}
+
+      <div className='homePageBackgroundOfEarth'>
+        <video className='videoTag' autoPlay loop muted>
+          <source src={homeBackground} type='video/mp4' />
+        </video>
+      </div>
+
+      <HomePageText />
+
+      <BusinessStrength />
+
+      <CombineRing />
+
+      <CoreCompitancy />
+
+      <Footer />
+      
+
+      
+      
+    </div>
+  );
+}
+
+export default HomePage;
