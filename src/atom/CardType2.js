c9529f0011dfbff7dd71a39e7a5eb28cc459a8b3
Tushar975868 <tusharsharma975@yahoo.com>
2024-08-03 15:50:44 +0530

    our client page ready

--- src/atom/CardType2.js
+++ src/atom/CardType2.js
@@ -4,9 +4,9 @@ import "./css/CardType2.style.css";
 const CardType2 = (props) => {
 
     return(
-        <div className="CardType2" style={{background: `url(${props.image})`, backgroundSize: "cover", }}>
+        <div className="CardType2" style={{background: `url(${props.image})`, backgroundSize: "cover", width: props.width, height: props.height}}>
 
-            <span className="cardType2Heading" >{props.heading}</span>
+            <span className="cardType2Heading" style={{color: props.color, fontSize: props.size, fontWeight: props.weight, paddingTop: props.paddingTop}}>{props.heading}</span>
 
             <span className="cardType2Details">{props.detail}</span>
         </div>
